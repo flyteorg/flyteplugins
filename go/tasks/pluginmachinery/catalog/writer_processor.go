74af2fcdee31b82941893df591464f4ea7259ae7
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2019-09-03 15:25:40 -0700

    remove v1 everywhere

---
+++ go/tasks/pluginmachinery/catalog/writer_processor.go
@@ -0,0 +1,66 @@
+package catalog
+
+import (
+	"context"
+	"fmt"
+	"github.com/lyft/flyteplugins/go/tasks/errors"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/io"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/workqueue"
+	"github.com/lyft/flytestdlib/logger"
+)
+
+type WriterWorkItem struct {
+	id workqueue.WorkItemID
+
+	// WriterWorkItem outputs
+	workStatus workqueue.WorkStatus
+
+	// WriterWorkItem Inputs
+	key      Key
+	data     io.OutputReader
+	metadata Metadata
+}
+
+func (item *WriterWorkItem) GetId() workqueue.WorkItemID {
+	return item.id
+}
+
+func (item *WriterWorkItem) GetWorkStatus() workqueue.WorkStatus {
+	return item.workStatus
+}
+
+func NewWriterWorkItem(id workqueue.WorkItemID, key Key, data io.OutputReader, metadata Metadata) WriterWorkItem {
+	return WriterWorkItem{
+		id:       id,
+		key:      key,
+		data:     data,
+		metadata: metadata,
+	}
+}
+
+type writerProcessor struct {
+	catalogClient Client
+}
+
+func (p writerProcessor) Process(ctx context.Context, workItem workqueue.WorkItem) (workqueue.WorkStatus, error) {
+	wi, casted := workItem.(*WriterWorkItem)
+	if !casted {
+		return workqueue.WorkStatusNotDone, fmt.Errorf("wrong work item type")
+	}
+
+	err := p.catalogClient.Put(ctx, wi.key, wi.data, wi.metadata)
+	if err != nil {
+		logger.Errorf(ctx, "Error putting to catalog [%s]", err)
+		return workqueue.WorkStatusNotDone, errors.Wrapf(errors.DownstreamSystemError, err,
+			"Error writing [%s] to catalog, key id [%s] cache version [%s]",
+			wi.id, Identifier, CacheVersion)
+	}
+
+	return workqueue.WorkStatusDone, nil
+}
+
+func NewWriterProcessor(catalogClient Client) workqueue.Processor {
+	return writerProcessor{
+		catalogClient: catalogClient,
+	}
+}
