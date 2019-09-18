e24b3e8831f5046f533f2f11c28cd42c753f0dbd
Yee Hing Tong <ytong@lyft.com>
2019-09-17 17:45:56 -0700

    Move Hive plugin (#12)
    Move hive plugin under plugins/ folder.

--- go/tasks/plugins/hive/executions_cache_test.go
+++ go/tasks/plugins/hive/executions_cache_test.go
@@ -2,8 +2,8 @@ package hive
 
 import (
 	"context"
-	"github.com/lyft/flyteplugins/go/tasks/hive/client"
-	quboleMocks "github.com/lyft/flyteplugins/go/tasks/hive/client/mocks"
+	"github.com/lyft/flyteplugins/go/tasks/plugins/hive/client"
+	quboleMocks "github.com/lyft/flyteplugins/go/tasks/plugins/hive/client/mocks"
 	"github.com/lyft/flytestdlib/promutils"
 	"github.com/lyft/flytestdlib/utils"
 	stdlibMocks "github.com/lyft/flytestdlib/utils/mocks"
