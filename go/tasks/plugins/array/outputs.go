df6e5026f836d29c1e7ee42a2b5375f800c1d007
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2019-10-02 10:16:19 -0700

    Change collected outputs of arrays to a multiple lists

--- go/tasks/plugins/array/outputs.go
+++ go/tasks/plugins/array/outputs.go
@@ -3,18 +3,40 @@ package array
 import (
 	"context"
 
+	"github.com/lyft/flyteidl/clients/go/coreutils"
+
 	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/io"
 	"github.com/lyft/flyteplugins/go/tasks/plugins/array/errorcollector"
 
 	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/ioutils"
 
-	core2 "github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
-	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/core"
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
+	pluginCore "github.com/lyft/flyteplugins/go/tasks/pluginmachinery/core"
 )
+
+func appendSubTaskOutput(outputs map[string]interface{}, subTaskOutput *core.LiteralMap,
+	expectedSize int64) {
+
+	for key, val := range subTaskOutput.GetLiterals() {
+		arr, exists := outputs[key]
+		if !exists {
+			arr = make([]*core.Literal, 0, expectedSize)
+		}
+
+		arr = append(arr.([]*core.Literal), val)
+		outputs[key] = arr
+	}
+}
+
+func appendEmptyOutputs(outputs map[string]interface{}) {
+	for key, val := range outputs {
+		outputs[key] = append(val.([]*core.Literal), &core.Literal{})
+	}
+}
 
 // Assembles a single outputs.pb that contain all the outputs of the subtasks and write them to the final OutputWriter.
 // This step can potentially be expensive (hence the metrics) and should be offloaded to a separate step.
-func AssembleFinalOutputs(ctx context.Context, tCtx core.TaskExecutionContext, state State) (State, error) {
+func AssembleFinalOutputs(ctx context.Context, tCtx pluginCore.TaskExecutionContext, state State) (State, error) {
 	// Otherwise, run the data catalog steps - create and submit work items to the catalog processor,
 	// build input readers
 	outputReaders, err := ConstructOutputReaders(ctx, tCtx.DataStore(), tCtx.OutputWriter().GetOutputPrefixPath(),
@@ -23,9 +45,10 @@ func AssembleFinalOutputs(ctx context.Context, tCtx core.TaskExecutionContext, s
 		return state, err
 	}
 
-	finalOutputs := make([]*core2.Literal, 0, state.GetOriginalArraySize())
+	finalOutputs := map[string]interface{}{}
+	//finalOutputs := make([]*core.Literal, 0, state.GetOriginalArraySize())
 	for idx, subTaskPhaseIdx := range state.GetArrayStatus().Detailed.GetItems() {
-		existingPhase := core.Phases[subTaskPhaseIdx]
+		existingPhase := pluginCore.Phases[subTaskPhaseIdx]
 		if existingPhase.IsSuccess() {
 			output, executionError, err := outputReaders[idx].Read(ctx)
 			if err != nil {
@@ -33,33 +56,21 @@ func AssembleFinalOutputs(ctx context.Context, tCtx core.TaskExecutionContext, s
 			}
 
 			if executionError == nil && output != nil {
-				finalOutputs = append(finalOutputs, &core2.Literal{
-					Value: &core2.Literal_Map{
-						Map: output,
-					},
-				})
+				appendSubTaskOutput(finalOutputs, output, state.GetOriginalArraySize())
+				continue
 			}
-		} else {
-			// TODO: Do we need the names of the outputs in the literalMap here?
-			finalOutputs = append(finalOutputs, &core2.Literal{
-				Value: &core2.Literal_Map{
-					Map: &core2.LiteralMap{},
-				},
-			})
 		}
+
+		// TODO: Do we need the names of the outputs in the literalMap here?
+		appendEmptyOutputs(finalOutputs)
 	}
 
-	err = tCtx.OutputWriter().Put(ctx, ioutils.NewInMemoryOutputReader(&core2.LiteralMap{
-		Literals: map[string]*core2.Literal{
-			"arr": {
-				Value: &core2.Literal_Collection{
-					Collection: &core2.LiteralCollection{
-						Literals: finalOutputs,
-					},
-				},
-			},
-		},
-	}, nil))
+	outputs, err := coreutils.MakeLiteralForMap(finalOutputs)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tCtx.OutputWriter().Put(ctx, ioutils.NewInMemoryOutputReader(outputs.GetMap(), nil))
 
 	if err != nil {
 		return nil, err
@@ -71,7 +82,7 @@ func AssembleFinalOutputs(ctx context.Context, tCtx core.TaskExecutionContext, s
 
 // Assembles a single error.pb that contain all the errors of the subtasks and write them to the final OutputWriter.
 // This step can potentially be expensive (hence the metrics) and should be offloaded to a separate step.
-func AssembleFinalErrors(ctx context.Context, tCtx core.TaskExecutionContext, maxErrorMessageLength int, state State) (State, error) {
+func AssembleFinalErrors(ctx context.Context, tCtx pluginCore.TaskExecutionContext, maxErrorMessageLength int, state State) (State, error) {
 	// Otherwise, run the data catalog steps - create and submit work items to the catalog processor,
 	// build input readers
 	outputReaders, err := ConstructOutputReaders(ctx, tCtx.DataStore(), tCtx.OutputWriter().GetOutputPrefixPath(),
@@ -82,7 +93,7 @@ func AssembleFinalErrors(ctx context.Context, tCtx core.TaskExecutionContext, ma
 
 	ec := errorcollector.NewErrorMessageCollector()
 	for idx, subTaskPhaseIdx := range state.GetArrayStatus().Detailed.GetItems() {
-		existingPhase := core.Phases[subTaskPhaseIdx]
+		existingPhase := pluginCore.Phases[subTaskPhaseIdx]
 		if existingPhase.IsFailure() {
 			_, executionError, err := outputReaders[idx].Read(ctx)
 			if err != nil {
@@ -97,7 +108,7 @@ func AssembleFinalErrors(ctx context.Context, tCtx core.TaskExecutionContext, ma
 
 	if ec.Length() > 0 {
 		err = tCtx.OutputWriter().Put(ctx, ioutils.NewInMemoryOutputReader(nil, &io.ExecutionError{
-			ExecutionError: &core2.ExecutionError{
+			ExecutionError: &core.ExecutionError{
 				Code:     "",
 				Message:  ec.Summary(maxErrorMessageLength),
 				ErrorUri: "",
