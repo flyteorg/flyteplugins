0cecdaca90a25a442a19b3822da7cc324b116517
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2019-10-16 00:53:30 -0700

    ensure log links even for terminal phases

--- go/tasks/plugins/array/k8s/monitor.go
+++ go/tasks/plugins/array/k8s/monitor.go
@@ -3,22 +3,23 @@ package k8s
 import (
 	"context"
 	"fmt"
-	arrayCore "github.com/lyft/flyteplugins/go/tasks/plugins/array/core"
 	"strconv"
 	"time"
 
-	arraystatus2 "github.com/lyft/flyteplugins/go/tasks/plugins/array/arraystatus"
-	errorcollector2 "github.com/lyft/flyteplugins/go/tasks/plugins/array/errorcollector"
+	arrayCore "github.com/lyft/flyteplugins/go/tasks/plugins/array/core"
+
+	"github.com/lyft/flyteplugins/go/tasks/plugins/array/arraystatus"
+	"github.com/lyft/flyteplugins/go/tasks/plugins/array/errorcollector"
 	"github.com/lyft/flytestdlib/bitarray"
 
 	v1 "k8s.io/api/core/v1"
-	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	types2 "k8s.io/apimachinery/pkg/types"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sTypes "k8s.io/apimachinery/pkg/types"
 
 	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/flytek8s"
 
-	core2 "github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
-	errors2 "github.com/lyft/flytestdlib/errors"
+	idlCore "github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
+	"github.com/lyft/flytestdlib/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/lyft/flyteplugins/go/tasks/logs"
@@ -26,18 +27,18 @@ import (
 )
 
 const (
-	ErrCheckPodStatus errors2.ErrorCode = "CHECK_POD_FAILED"
+	ErrCheckPodStatus errors.ErrorCode = "CHECK_POD_FAILED"
 )
 
 func CheckSubTasksState(ctx context.Context, tCtx core.TaskExecutionContext, kubeClient core.KubeClient,
-	cfg *Config, currentState *arrayCore.State) (newState *arrayCore.State, err error) {
+	cfg *Config, currentState *arrayCore.State) (newState *arrayCore.State, logLinks []*idlCore.TaskLog, err error) {
 
-	logLinks := make([]*core2.TaskLog, 0, 4)
+	logLinks = make([]*idlCore.TaskLog, 0, 4)
 	newState = currentState
 
-	msg := errorcollector2.NewErrorMessageCollector()
-	newArrayStatus := arraystatus2.ArrayStatus{
-		Summary:  arraystatus2.ArraySummary{},
+	msg := errorcollector.NewErrorMessageCollector()
+	newArrayStatus := arraystatus.ArrayStatus{
+		Summary:  arraystatus.ArraySummary{},
 		Detailed: arrayCore.NewPhasesCompactArray(uint(currentState.GetExecutionArraySize())),
 	}
 
@@ -54,12 +55,12 @@ func CheckSubTasksState(ctx context.Context, tCtx core.TaskExecutionContext, kub
 		}
 
 		phaseInfo, err := CheckPodStatus(ctx, kubeClient,
-			types2.NamespacedName{
+			k8sTypes.NamespacedName{
 				Name:      formatSubTaskName(ctx, tCtx.TaskExecutionMetadata().GetTaskExecutionID().GetGeneratedName(), strconv.Itoa(childIdx)),
 				Namespace: tCtx.TaskExecutionMetadata().GetNamespace(),
 			})
 		if err != nil {
-			return currentState, errors2.Wrapf(ErrCheckPodStatus, err, "Failed to check pod status")
+			return currentState, logLinks, errors.Wrapf(ErrCheckPodStatus, err, "Failed to check pod status")
 		}
 
 		if phaseInfo.Info() != nil {
@@ -79,9 +80,9 @@ func CheckSubTasksState(ctx context.Context, tCtx core.TaskExecutionContext, kub
 	// Check that the taskTemplate is valid
 	taskTemplate, err := tCtx.TaskReader().Read(ctx)
 	if err != nil {
-		return currentState, err
+		return currentState, logLinks, err
 	} else if taskTemplate == nil {
-		return currentState, fmt.Errorf("required value not set, taskTemplate is nil")
+		return currentState, logLinks, fmt.Errorf("required value not set, taskTemplate is nil")
 	}
 
 	phase := arrayCore.SummaryToPhase(ctx, currentState.GetOriginalMinSuccesses()-currentState.GetOriginalArraySize()+int64(currentState.GetExecutionArraySize()), newArrayStatus.Summary)
@@ -104,14 +105,14 @@ func CheckSubTasksState(ctx context.Context, tCtx core.TaskExecutionContext, kub
 		newState = newState.SetPhase(phase, core.DefaultPhaseVersion)
 	}
 
-	return newState, nil
+	return newState, logLinks, nil
 }
 
-func CheckPodStatus(ctx context.Context, client core.KubeClient, name types2.NamespacedName) (
+func CheckPodStatus(ctx context.Context, client core.KubeClient, name k8sTypes.NamespacedName) (
 	info core.PhaseInfo, err error) {
 
 	pod := &v1.Pod{
-		TypeMeta: v12.TypeMeta{
+		TypeMeta: metaV1.TypeMeta{
 			Kind:       PodKind,
 			APIVersion: v1.SchemeGroupVersion.String(),
 		},
@@ -124,7 +125,7 @@ func CheckPodStatus(ctx context.Context, client core.KubeClient, name types2.Nam
 		if k8serrors.IsNotFound(err) {
 			// If the object disappeared at this point, it means it was manually removed or garbage collected.
 			// Mark it as a failure.
-			return core.PhaseInfoFailed(core.PhaseRetryableFailure, &core2.ExecutionError{
+			return core.PhaseInfoFailed(core.PhaseRetryableFailure, &idlCore.ExecutionError{
 				Code:    string(k8serrors.ReasonForError(err)),
 				Message: err.Error(),
 			}, &core.TaskInfo{
