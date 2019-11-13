a43fd38bd0725e3ada575e187878c75b0470d5a6
Ketan Umare <kumare@lyft.com>
2019-11-13 10:19:44 -0800

    Updated mocks

--- go/tasks/pluginmachinery/service/plugin.go
+++ go/tasks/pluginmachinery/service/plugin.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"time"
 
 	"github.com/lyft/flytestdlib/promutils"
 
@@ -55,42 +54,6 @@ type RemoteResourceKey struct {
 
 // The resource to be sycned from the remote
 type RemoteResource interface{}
-
-// The plugin manager automatically queries the remote API
-type RateLimiterProperties struct {
-	// Queries per second from one process to the remote service
-	QPS int
-	// Maximum burst size
-	Burst int
-}
-
-type CachingProperties struct {
-	// Max number of RemoteResource's to be stored in the local cache
-	Size int
-	// How often to query for objects in remote service.
-	ResyncInterval time.Duration
-}
-
-type MissingResourceSemantics uint8
-
-const (
-	MissingResourceFailIfMissing MissingResourceSemantics = iota
-	MissingResourceRetryIfMissing
-)
-
-// Properties that indicate if the Batch API can be invoked. This indicates that GetBatch() can be used instead of Get
-type BatchingProperties struct {
-	BatchAPISupported        bool
-	MaxBatchSize             int
-	MissingResourceSemantics MissingResourceSemantics
-}
-
-// Properties that help the system optimize itself to handle the specific plugin
-type PluginProperties struct {
-	RateLimiterProperties RateLimiterProperties
-	CachingProperties     CachingProperties
-	BatchingProperties    BatchingProperties
-}
 
 // Defines a simplified interface to author plugins for k8s resources.
 type Plugin interface {
