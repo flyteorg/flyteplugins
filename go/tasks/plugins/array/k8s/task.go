9cd4e4e601085f5f7b049064ed55b7387c8b62e3
Miguel Toledo <mtoledo@lyft.com>
2020-04-06 11:12:32 -0700

    switch to enum return status

--- go/tasks/plugins/array/k8s/task.go
+++ go/tasks/plugins/array/k8s/task.go
@@ -22,11 +22,20 @@ type Task struct {
 	ChildIdx int
 }
 
-func (t Task) Launch(ctx context.Context, tCtx core.TaskExecutionContext, kubeClient core.KubeClient) (bool, error) {
+type TaskLaunchStatus int8
+
+const (
+	Success TaskLaunchStatus = iota
+	Error
+	Skip
+	ReturnState
+)
+
+func (t Task) Launch(ctx context.Context, tCtx core.TaskExecutionContext, kubeClient core.KubeClient) (TaskLaunchStatus, error) {
 	newState := t.State
 	podTemplate, _, err := FlyteArrayJobToK8sPodTemplate(ctx, tCtx)
 	if err != nil {
-		return false, errors2.Wrapf(ErrBuildPodTemplate, err, "Failed to convert task template to a pod template for a task")
+		return Error, errors2.Wrapf(ErrBuildPodTemplate, err, "Failed to convert task template to a pod template for a task")
 	}
 
 	var args []string
@@ -48,18 +57,18 @@ func (t Task) Launch(ctx context.Context, tCtx core.TaskExecutionContext, kubeCl
 	pod.Spec.Containers[0].Env = append(pod.Spec.Containers[0].Env, arrayJobEnvVars...)
 	pod.Spec.Containers[0].Args, err = utils.ReplaceTemplateCommandArgs(ctx, args, arrayJobInputReader{tCtx.InputReader()}, tCtx.OutputWriter())
 	if err != nil {
-		return false, errors2.Wrapf(ErrReplaceCmdTemplate, err, "Failed to replace cmd args")
+		return Error, errors2.Wrapf(ErrReplaceCmdTemplate, err, "Failed to replace cmd args")
 	}
 
 	pod = ApplyPodPolicies(ctx, t.Config, pod)
 
 	succeeded, err := allocateResource(ctx, tCtx, t.Config, podName, t.ChildIdx, &newState.ArrayStatus)
 	if err != nil {
-		return succeeded, err
+		return Error, err
 	}
 
 	if !succeeded {
-		return false, nil
+		return Skip, nil
 	}
 
 	err = kubeClient.GetClient().Create(ctx, pod)
@@ -74,10 +83,11 @@ func (t Task) Launch(ctx context.Context, tCtx core.TaskExecutionContext, kubeCl
 			}
 
 			newState = newState.SetReason(err.Error())
-			return false, nil
+			return ReturnState, nil
 		}
 
-		return false, errors2.Wrapf(ErrSubmitJob, err, "Failed to submit job.")
+		return Error, errors2.Wrapf(ErrSubmitJob, err, "Failed to submit job.")
 	}
 
+	return Success, nil
 }
