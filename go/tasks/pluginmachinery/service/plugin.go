6a0594a018d4304c8bd7a5a0f3ff105a12dae841
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-04-13 17:50:13 -0700

    PR Comments

--- go/tasks/pluginmachinery/service/plugin.go
+++ go/tasks/pluginmachinery/service/plugin.go
@@ -60,15 +60,11 @@ type Plugin interface {
 	GetPluginProperties() PluginProperties
 	// Create a new resource using the PluginContext provided. Ideally, the remote service uses the name in the TaskExecutionMetadata
 	// to launch the resource idempotently
-	Create(ctx context.Context, tCtx PluginContext) (RemoteResourceKey, error)
-	// Get the entire object including the status from the remote api
-	Get(ctx context.Context, key RemoteResourceKey) (RemoteResource, error)
-	// Get multiple resources that match all the keys. This method is optional and should be provided in case the
-	// PluginProperties.BatchingProperties.BatchAPISupported = true
-	GetBatch(ctx context.Context, key ...RemoteResourceKey) (RemoteResource, error)
+	Create(ctx context.Context, tCtx PluginContext) (createdResources []RemoteResourceKey, err error)
+	// Get multiple resources that match all the keys.
+	Get(ctx context.Context, key ...RemoteResourceKey) (resources []RemoteResource, err error)
 	// Delete the object in the remote API using the resource key
-	Delete(ctx context.Context, key RemoteResourceKey) error
-	// Give the resource as interface{} (as Golang has no support for generics) we return a type unsafe interface, which should be typecasted
-	// to the actual remote object type and transformed to a PhaseInfo
-	GetStatus(ctx context.Context, tCtx PluginContext, resource RemoteResource) (pluginsCore.PhaseInfo, error)
+	Delete(ctx context.Context, key ...RemoteResourceKey) error
+	// Status checks the status of a given resource and translates it to a Flyte-understandable PhaseInfo.
+	Status(ctx context.Context, tCtx PluginContext, resource RemoteResource) (phase pluginsCore.PhaseInfo, err error)
 }
