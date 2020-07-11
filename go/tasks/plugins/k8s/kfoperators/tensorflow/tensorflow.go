37083ed24fe9b9a7c6e709f399729d63711ef47a
pingsutw <pingsutw@gmail.com>
2020-07-12 01:19:50 +0800

    tensorflow plugin implementation

---
+++ go/tasks/plugins/k8s/kfoperators/tensorflow/tensorflow.go
@@ -0,0 +1,251 @@
+package tensorflow
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/plugins"
+	flyteerr "github.com/lyft/flyteplugins/go/tasks/errors"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/flytek8s"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+
+	pluginsCore "github.com/lyft/flyteplugins/go/tasks/pluginmachinery/core"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/k8s"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/utils"
+
+	logUtils "github.com/lyft/flyteidl/clients/go/coreutils/logs"
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
+	"github.com/lyft/flyteplugins/go/tasks/logs"
+
+	//commonOp "github.com/kubeflow/common/pkg/apis/common/v1" // switch to real 'common' once https://github.com/kubeflow/pytorch-operator/issues/263 resolved
+	commonOp "github.com/kubeflow/tf-operator/pkg/apis/common/v1"
+	tfOp "github.com/kubeflow/tf-operator/pkg/apis/tensorflow/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	tensorflowTaskType = "tensorflow"
+)
+
+type tensorflowOperatorResourceHandler struct {
+}
+
+// Sanity test that the plugin implements method of k8s.Plugin
+var _ k8s.Plugin = tensorflowOperatorResourceHandler{}
+
+// Defines a func to create a query object (typically just object and type meta portions) that's used to query k8s
+// resources.
+func (tensorflowOperatorResourceHandler) BuildIdentityResource(ctx context.Context, taskCtx pluginsCore.TaskExecutionMetadata) (k8s.Resource, error) {
+	return &tfOp.TFJob{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       tfOp.Kind,
+			APIVersion: tfOp.SchemeGroupVersion.String(),
+		},
+	}, nil
+}
+
+// Defines a func to create the full resource object that will be posted to k8s.
+func (tensorflowOperatorResourceHandler) BuildResource(ctx context.Context, taskCtx pluginsCore.TaskExecutionContext) (k8s.Resource, error) {
+	taskTemplate, err := taskCtx.TaskReader().Read(ctx)
+
+	if err != nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "unable to fetch task specification [%v]", err.Error())
+	} else if taskTemplate == nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "nil task specification")
+	}
+
+	tensorflowTaskExtraArgs := plugins.DistributedTensorflowTrainingTask{}
+	err = utils.UnmarshalStruct(taskTemplate.GetCustom(), &tensorflowTaskExtraArgs)
+	if err != nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "invalid TaskSpecification [%v], Err: [%v]", taskTemplate.GetCustom(), err.Error())
+	}
+
+	podSpec, err := flytek8s.ToK8sPodSpec(ctx, taskCtx.TaskExecutionMetadata(), taskCtx.TaskReader(), taskCtx.InputReader(), taskCtx.OutputWriter())
+	if err != nil {
+		return nil, flyteerr.Errorf(flyteerr.BadTaskSpecification, "Unable to create pod spec: [%v]", err.Error())
+	}
+
+	overrideDefaultContainerName(taskCtx, podSpec)
+
+	workers := tensorflowTaskExtraArgs.GetWorkers()
+	psReplicas := tensorflowTaskExtraArgs.GetPsReplicas()
+	chiefReplicas := tensorflowTaskExtraArgs.GetChiefReplicas()
+
+	jobSpec := tfOp.TFJobSpec{
+		TTLSecondsAfterFinished: nil,
+		TFReplicaSpecs: map[tfOp.TFReplicaType]*commonOp.ReplicaSpec{
+			tfOp.TFReplicaTypePS: {
+				Replicas: &psReplicas,
+				Template: v1.PodTemplateSpec{
+					Spec: *podSpec,
+				},
+				RestartPolicy: commonOp.RestartPolicyNever,
+			},
+			tfOp.TFReplicaTypeChief: {
+				Replicas: &chiefReplicas,
+				Template: v1.PodTemplateSpec{
+					Spec: *podSpec,
+				},
+				RestartPolicy: commonOp.RestartPolicyNever,
+			},
+			tfOp.TFReplicaTypeWorker: {
+				Replicas: &workers,
+				Template: v1.PodTemplateSpec{
+					Spec: *podSpec,
+				},
+				RestartPolicy: commonOp.RestartPolicyNever,
+			},
+		},
+	}
+
+	job := &tfOp.TFJob{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       tfOp.Kind,
+			APIVersion: tfOp.SchemeGroupVersion.String(),
+		},
+		Spec: jobSpec,
+	}
+
+	return job, nil
+}
+
+// Analyses the k8s resource and reports the status as TaskPhase. This call is expected to be relatively fast,
+// any operations that might take a long time (limits are configured system-wide) should be offloaded to the
+// background.
+func (tensorflowOperatorResourceHandler) GetTaskPhase(ctx context.Context, pluginContext k8s.PluginContext, resource k8s.Resource) (pluginsCore.PhaseInfo, error) {
+	app := resource.(*tfOp.TFJob)
+
+	workersCount := app.Spec.TFReplicaSpecs[tfOp.TFReplicaTypeWorker].Replicas
+	psReplicasCount := app.Spec.TFReplicaSpecs[tfOp.TFReplicaTypePS].Replicas
+	chiefCount := app.Spec.TFReplicaSpecs[tfOp.TFReplicaTypeChief].Replicas
+
+	taskLogs, err := getLogs(app, *workersCount, *psReplicasCount, *chiefCount)
+	if err != nil {
+		return pluginsCore.PhaseInfoUndefined, err
+	}
+
+	currentCondition, err := extractCurrentCondition(app.Status.Conditions)
+	if err != nil {
+		return pluginsCore.PhaseInfoUndefined, err
+	}
+
+	occurredAt := time.Now()
+	statusDetails, _ := utils.MarshalObjToStruct(app.Status)
+	taskPhaseInfo := pluginsCore.TaskInfo{
+		Logs:       taskLogs,
+		OccurredAt: &occurredAt,
+		CustomInfo: statusDetails,
+	}
+
+	switch currentCondition.Type {
+	case commonOp.JobCreated:
+		return pluginsCore.PhaseInfoQueued(occurredAt, pluginsCore.DefaultPhaseVersion, "JobCreated"), nil
+	case commonOp.JobRunning:
+		return pluginsCore.PhaseInfoRunning(pluginsCore.DefaultPhaseVersion, &taskPhaseInfo), nil
+	case commonOp.JobSucceeded:
+		return pluginsCore.PhaseInfoSuccess(&taskPhaseInfo), nil
+	case commonOp.JobFailed:
+		details := fmt.Sprintf("Job failed:\n\t%v - %v", currentCondition.Reason, currentCondition.Message)
+		return pluginsCore.PhaseInfoRetryableFailure(flyteerr.DownstreamSystemError, details, &taskPhaseInfo), nil
+	case commonOp.JobRestarting:
+		details := fmt.Sprintf("Job failed:\n\t%v - %v", currentCondition.Reason, currentCondition.Message)
+		return pluginsCore.PhaseInfoRetryableFailure(flyteerr.RuntimeFailure, details, &taskPhaseInfo), nil
+	}
+
+	return pluginsCore.PhaseInfoUndefined, nil
+}
+
+func getLogs(app *tfOp.TFJob, workersCount int32, psReplicasCount int32, chiefReplicasCount int32) ([]*core.TaskLog, error) {
+	// If kubeClient was available, it would be better to use
+	// https://github.com/lyft/flyteplugins/blob/209c52d002b4e6a39be5d175bc1046b7e631c153/go/tasks/logs/logging_utils.go#L12
+	makeTaskLog := func(appName, appNamespace, suffix, url string) (core.TaskLog, error) {
+		return logUtils.NewKubernetesLogPlugin(url).GetTaskLog(
+			appName+"-"+suffix,
+			appNamespace,
+			"",
+			"",
+			suffix+" logs (via Kubernetes)")
+	}
+
+	var taskLogs []*core.TaskLog
+
+	logConfig := logs.GetLogConfig()
+	if logConfig.IsKubernetesEnabled {
+		masterTaskLog, masterErr := makeTaskLog(app.Name, app.Namespace, "master-0", logConfig.KubernetesURL)
+		if masterErr != nil {
+			return nil, masterErr
+		}
+		taskLogs = append(taskLogs, &masterTaskLog)
+
+		// get all workers log
+		for workerIndex := int32(0); workerIndex < workersCount; workerIndex++ {
+			workerLog, err := makeTaskLog(app.Name, app.Namespace, fmt.Sprintf("worker-%d", workerIndex), logConfig.KubernetesURL)
+			if err != nil {
+				return nil, err
+			}
+			taskLogs = append(taskLogs, &workerLog)
+		}
+		// get all parameter servers logs
+		for psReplicaIndex := int32(0); psReplicaIndex < psReplicasCount; psReplicaIndex++ {
+			psReplicaLog, err := makeTaskLog(app.Name, app.Namespace, fmt.Sprintf("psReplica-%d", psReplicaIndex), logConfig.KubernetesURL)
+			if err != nil {
+				return nil, err
+			}
+			taskLogs = append(taskLogs, &psReplicaLog)
+		}
+		// get chief worker log, and the max number of chief worker is 1
+		chiefReplicaLog, err := makeTaskLog(app.Name, app.Namespace, fmt.Sprintf("chiefReplica-%d", 0), logConfig.KubernetesURL)
+		if err != nil {
+			return nil, err
+		}
+		taskLogs = append(taskLogs, &chiefReplicaLog)
+	}
+	return taskLogs, nil
+}
+
+func extractCurrentCondition(jobConditions []commonOp.JobCondition) (commonOp.JobCondition, error) {
+	sort.Slice(jobConditions[:], func(i, j int) bool {
+		return jobConditions[i].LastTransitionTime.Time.After(jobConditions[j].LastTransitionTime.Time)
+	})
+
+	for _, jc := range jobConditions {
+		if jc.Status == v1.ConditionTrue {
+			return jc, nil
+		}
+	}
+
+	return commonOp.JobCondition{}, fmt.Errorf("found no current condition. Conditions: %+v", jobConditions)
+}
+
+func overrideDefaultContainerName(taskCtx pluginsCore.TaskExecutionContext, podSpec *v1.PodSpec) {
+	// Pytorch operator forces pod to have container named 'pytorch'
+	// https://github.com/kubeflow/pytorch-operator/blob/037cd1b18eb77f657f2a4bc8a8334f2a06324b57/pkg/apis/pytorch/validation/validation.go#L54-L62
+	// hence we have to override the name set here
+	// https://github.com/lyft/flyteplugins/blob/209c52d002b4e6a39be5d175bc1046b7e631c153/go/tasks/pluginmachinery/flytek8s/container_helper.go#L116
+	flyteDefaultContainerName := taskCtx.TaskExecutionMetadata().GetTaskExecutionID().GetGeneratedName()
+	for idx, c := range podSpec.Containers {
+		if c.Name == flyteDefaultContainerName {
+			podSpec.Containers[idx].Name = tfOp.DefaultContainerName
+			return
+		}
+	}
+}
+
+func init() {
+	if err := tfOp.AddToScheme(scheme.Scheme); err != nil {
+		panic(err)
+	}
+
+	pluginmachinery.PluginRegistry().RegisterK8sPlugin(
+		k8s.PluginEntry{
+			ID:                  tensorflowTaskType,
+			RegisteredTaskTypes: []pluginsCore.TaskType{tensorflowTaskType},
+			ResourceToWatch:     &tfOp.TFJob{},
+			Plugin:              tensorflowOperatorResourceHandler{},
+			IsDefault:           false,
+		})
+}
---
+++ go/tasks/plugins/k8s/kfoperators/tensorflow/tensorflow_test.go
@@ -0,0 +1,361 @@
+package tensorflow
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	commonOp "github.com/kubeflow/tf-operator/pkg/apis/common/v1"
+	"github.com/lyft/flyteplugins/go/tasks/logs"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/flytek8s"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	"github.com/stretchr/testify/mock"
+
+	"github.com/lyft/flytestdlib/storage"
+
+	pluginsCore "github.com/lyft/flyteplugins/go/tasks/pluginmachinery/core"
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/utils"
+
+	"github.com/lyft/flyteplugins/go/tasks/pluginmachinery/core/mocks"
+
+	pluginIOMocks "github.com/lyft/flyteplugins/go/tasks/pluginmachinery/io/mocks"
+
+	"github.com/golang/protobuf/jsonpb"
+	structpb "github.com/golang/protobuf/ptypes/struct"
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/plugins"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	tfOp "github.com/kubeflow/tf-operator/pkg/apis/tensorflow/v1"
+)
+
+const testImage = "image://"
+const serviceAccount = "tensorflow_sa"
+
+var (
+	dummyEnvVars = []*core.KeyValuePair{
+		{Key: "Env_Var", Value: "Env_Val"},
+	}
+
+	testArgs = []string{
+		"test-args",
+	}
+
+	resourceRequirements = &corev1.ResourceRequirements{
+		Limits: corev1.ResourceList{
+			corev1.ResourceCPU:         resource.MustParse("1000m"),
+			corev1.ResourceMemory:      resource.MustParse("1Gi"),
+			flytek8s.ResourceNvidiaGPU: resource.MustParse("1"),
+		},
+		Requests: corev1.ResourceList{
+			corev1.ResourceCPU:         resource.MustParse("100m"),
+			corev1.ResourceMemory:      resource.MustParse("512Mi"),
+			flytek8s.ResourceNvidiaGPU: resource.MustParse("1"),
+		},
+	}
+
+	jobName      = "the-job"
+	jobNamespace = "tensorflow-namespace"
+)
+
+func dummyTensorflowCustomObj(workers int32, psReplicas int32, chiefReplicas int32) *plugins.DistributedTensorflowTrainingTask {
+	return &plugins.DistributedTensorflowTrainingTask{
+		Workers: workers,
+		PsReplicas: psReplicas,
+		ChiefReplicas: chiefReplicas,
+	}
+}
+
+func dummySparkTaskTemplate(id string, tensorflowCustomObj *plugins.DistributedTensorflowTrainingTask) *core.TaskTemplate {
+
+	tfObjJSON, err := utils.MarshalToString(tensorflowCustomObj)
+	if err != nil {
+		panic(err)
+	}
+
+	structObj := structpb.Struct{}
+
+	err = jsonpb.UnmarshalString(tfObjJSON, &structObj)
+	if err != nil {
+		panic(err)
+	}
+
+	return &core.TaskTemplate{
+		Id:   &core.Identifier{Name: id},
+		Type: "container",
+		Target: &core.TaskTemplate_Container{
+			Container: &core.Container{
+				Image: testImage,
+				Args:  testArgs,
+				Env:   dummyEnvVars,
+			},
+		},
+		Custom: &structObj,
+	}
+}
+
+func dummyTensorflowTaskContext(taskTemplate *core.TaskTemplate) pluginsCore.TaskExecutionContext {
+	taskCtx := &mocks.TaskExecutionContext{}
+	inputReader := &pluginIOMocks.InputReader{}
+	inputReader.OnGetInputPrefixPath().Return(storage.DataReference("/input/prefix"))
+	inputReader.OnGetInputPath().Return(storage.DataReference("/input"))
+	inputReader.OnGetMatch(mock.Anything).Return(&core.LiteralMap{}, nil)
+	taskCtx.OnInputReader().Return(inputReader)
+
+	outputReader := &pluginIOMocks.OutputWriter{}
+	outputReader.OnGetOutputPath().Return(storage.DataReference("/data/outputs.pb"))
+	outputReader.OnGetOutputPrefixPath().Return(storage.DataReference("/data/"))
+	taskCtx.OnOutputWriter().Return(outputReader)
+
+	taskReader := &mocks.TaskReader{}
+	taskReader.OnReadMatch(mock.Anything).Return(taskTemplate, nil)
+	taskCtx.OnTaskReader().Return(taskReader)
+
+	tID := &mocks.TaskExecutionID{}
+	tID.OnGetID().Return(core.TaskExecutionIdentifier{
+		NodeExecutionId: &core.NodeExecutionIdentifier{
+			ExecutionId: &core.WorkflowExecutionIdentifier{
+				Name:    "my_name",
+				Project: "my_project",
+				Domain:  "my_domain",
+			},
+		},
+	})
+	tID.OnGetGeneratedName().Return("some-acceptable-name")
+
+	resources := &mocks.TaskOverrides{}
+	resources.OnGetResources().Return(resourceRequirements)
+
+	taskExecutionMetadata := &mocks.TaskExecutionMetadata{}
+	taskExecutionMetadata.OnGetTaskExecutionID().Return(tID)
+	taskExecutionMetadata.OnGetNamespace().Return("test-namespace")
+	taskExecutionMetadata.OnGetAnnotations().Return(map[string]string{"annotation-1": "val1"})
+	taskExecutionMetadata.OnGetLabels().Return(map[string]string{"label-1": "val1"})
+	taskExecutionMetadata.OnGetOwnerReference().Return(v1.OwnerReference{
+		Kind: "node",
+		Name: "blah",
+	})
+	taskExecutionMetadata.OnIsInterruptible().Return(true)
+	taskExecutionMetadata.OnGetOverrides().Return(resources)
+	taskExecutionMetadata.OnGetK8sServiceAccount().Return(serviceAccount)
+	taskCtx.OnTaskExecutionMetadata().Return(taskExecutionMetadata)
+	return taskCtx
+}
+
+func dummyTensorflowJobResource(tensorflowResourceHandler tensorflowOperatorResourceHandler,
+	workers int32, psReplicas int32, chiefReplicas int32, conditionType commonOp.JobConditionType) *tfOp.TFJob {
+	var jobConditions []commonOp.JobCondition
+
+	now := time.Now()
+
+	jobCreated := commonOp.JobCondition{
+		Type:    commonOp.JobCreated,
+		Status:  corev1.ConditionTrue,
+		Reason:  "TensorflowJobCreated",
+		Message: "TensorflowJob the-job is created.",
+		LastUpdateTime: v1.Time{
+			Time: now,
+		},
+		LastTransitionTime: v1.Time{
+			Time: now,
+		},
+	}
+	jobRunningActive := commonOp.JobCondition{
+		Type:    commonOp.JobRunning,
+		Status:  corev1.ConditionTrue,
+		Reason:  "TensorflowJobRunning",
+		Message: "TensorflowJob the-job is running.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(time.Minute),
+		},
+	}
+	jobRunningInactive := *jobRunningActive.DeepCopy()
+	jobRunningInactive.Status = corev1.ConditionFalse
+	jobSucceeded := commonOp.JobCondition{
+		Type:    commonOp.JobSucceeded,
+		Status:  corev1.ConditionTrue,
+		Reason:  "TensorflowJobSucceeded",
+		Message: "TensorflowJob the-job is successfully completed.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+	}
+	jobFailed := commonOp.JobCondition{
+		Type:    commonOp.JobFailed,
+		Status:  corev1.ConditionTrue,
+		Reason:  "TensorflowJobFailed",
+		Message: "TensorflowJob the-job is failed.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(2 * time.Minute),
+		},
+	}
+	jobRestarting := commonOp.JobCondition{
+		Type:    commonOp.JobRestarting,
+		Status:  corev1.ConditionTrue,
+		Reason:  "TensorflowJobRestarting",
+		Message: "TensorflowJob the-job is restarting because some replica(s) failed.",
+		LastUpdateTime: v1.Time{
+			Time: now.Add(3 * time.Minute),
+		},
+		LastTransitionTime: v1.Time{
+			Time: now.Add(3 * time.Minute),
+		},
+	}
+
+	switch conditionType {
+	case commonOp.JobCreated:
+		jobConditions = []commonOp.JobCondition{
+			jobCreated,
+		}
+	case commonOp.JobRunning:
+		jobConditions = []commonOp.JobCondition{
+			jobCreated,
+			jobRunningActive,
+		}
+	case commonOp.JobSucceeded:
+		jobConditions = []commonOp.JobCondition{
+			jobCreated,
+			jobRunningInactive,
+			jobSucceeded,
+		}
+	case commonOp.JobFailed:
+		jobConditions = []commonOp.JobCondition{
+			jobCreated,
+			jobRunningInactive,
+			jobFailed,
+		}
+	case commonOp.JobRestarting:
+		jobConditions = []commonOp.JobCondition{
+			jobCreated,
+			jobRunningInactive,
+			jobFailed,
+			jobRestarting,
+		}
+	}
+
+	tfObj := dummyTensorflowCustomObj(workers, psReplicas, chiefReplicas)
+	taskTemplate := dummySparkTaskTemplate("the job", tfObj)
+	resource, err := tensorflowResourceHandler.BuildResource(context.TODO(), dummyTensorflowTaskContext(taskTemplate))
+	if err != nil {
+		panic(err)
+	}
+
+	return &tfOp.TFJob{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      jobName,
+			Namespace: jobNamespace,
+		},
+		Spec: resource.(*tfOp.TFJob).Spec,
+		Status: commonOp.JobStatus{
+			Conditions:        jobConditions,
+			ReplicaStatuses:   nil,
+			StartTime:         nil,
+			CompletionTime:    nil,
+			LastReconcileTime: nil,
+		},
+	}
+}
+
+func TestBuildResourceTensorflow(t *testing.T) {
+	tensorflowResourceHandler := tensorflowOperatorResourceHandler{}
+
+	tfObj := dummyTensorflowCustomObj(100, 1,1)
+	taskTemplate := dummySparkTaskTemplate("the job", tfObj)
+
+	resource, err := tensorflowResourceHandler.BuildResource(context.TODO(), dummyTensorflowTaskContext(taskTemplate))
+	assert.NoError(t, err)
+	assert.NotNil(t, resource)
+
+	tensorflowJob, ok := resource.(*tfOp.TFJob)
+	assert.True(t, ok)
+	assert.Equal(t, int32(100), *tensorflowJob.Spec.TFReplicaSpecs[tfOp.TFReplicaTypeWorker].Replicas)
+	assert.Equal(t, int32(1), *tensorflowJob.Spec.TFReplicaSpecs[tfOp.TFReplicaTypePS].Replicas)
+	assert.Equal(t, int32(1), *tensorflowJob.Spec.TFReplicaSpecs[tfOp.TFReplicaTypeChief].Replicas)
+
+	for _, replicaSpec := range tensorflowJob.Spec.TFReplicaSpecs {
+		var hasContainerWithDefaultTensorflowName = false
+
+		for _, container := range replicaSpec.Template.Spec.Containers {
+			if container.Name == tfOp.DefaultContainerName {
+				hasContainerWithDefaultTensorflowName = true
+			}
+
+			assert.Equal(t, resourceRequirements.Requests, container.Resources.Requests)
+			assert.Equal(t, resourceRequirements.Limits, container.Resources.Limits)
+		}
+
+		assert.True(t, hasContainerWithDefaultTensorflowName)
+	}
+}
+
+func TestGetTaskPhase(t *testing.T) {
+	tensorflowResourceHandler := tensorflowOperatorResourceHandler{}
+	ctx := context.TODO()
+
+	dummyTensorflowJobResourceCreator := func(conditionType commonOp.JobConditionType) *tfOp.TFJob {
+		return dummyTensorflowJobResource(tensorflowResourceHandler, 2,1, 1, conditionType)
+	}
+
+	taskPhase, err := tensorflowResourceHandler.GetTaskPhase(ctx, nil, dummyTensorflowJobResourceCreator(commonOp.JobCreated))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseQueued, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = tensorflowResourceHandler.GetTaskPhase(ctx, nil, dummyTensorflowJobResourceCreator(commonOp.JobRunning))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseRunning, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = tensorflowResourceHandler.GetTaskPhase(ctx, nil, dummyTensorflowJobResourceCreator(commonOp.JobSucceeded))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseSuccess, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = tensorflowResourceHandler.GetTaskPhase(ctx, nil, dummyTensorflowJobResourceCreator(commonOp.JobFailed))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseRetryableFailure, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+
+	taskPhase, err = tensorflowResourceHandler.GetTaskPhase(ctx, nil, dummyTensorflowJobResourceCreator(commonOp.JobRestarting))
+	assert.NoError(t, err)
+	assert.Equal(t, pluginsCore.PhaseRetryableFailure, taskPhase.Phase())
+	assert.NotNil(t, taskPhase.Info())
+	assert.Nil(t, err)
+}
+
+func TestGetLogs(t *testing.T) {
+	assert.NoError(t, logs.SetLogConfig(&logs.LogConfig{
+		IsKubernetesEnabled: true,
+		KubernetesURL:       "k8s.com",
+	}))
+
+	workers := int32(2)
+	psReplicas := int32(1)
+	chiefReplicas := int32(1)
+
+	tensorflowResourceHandler := tensorflowOperatorResourceHandler{}
+	jobLogs, err := getLogs(dummyTensorflowJobResource(tensorflowResourceHandler, workers, psReplicas, chiefReplicas, commonOp.JobRunning),
+		workers, psReplicas, chiefReplicas)
+	assert.NoError(t, err)
+	assert.Equal(t, 5, len(jobLogs))
+	assert.Equal(t, fmt.Sprintf("k8s.com/#!/log/%s/%s-master-0/pod?namespace=tensorflow-namespace", jobNamespace, jobName), jobLogs[0].Uri)
+	assert.Equal(t, fmt.Sprintf("k8s.com/#!/log/%s/%s-worker-0/pod?namespace=tensorflow-namespace", jobNamespace, jobName), jobLogs[1].Uri)
+	assert.Equal(t, fmt.Sprintf("k8s.com/#!/log/%s/%s-worker-1/pod?namespace=tensorflow-namespace", jobNamespace, jobName), jobLogs[2].Uri)
+}
