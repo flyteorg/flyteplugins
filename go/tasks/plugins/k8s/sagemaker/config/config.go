56df3b5639a5c7e4d82f654b1fd74b2da4e5393d
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-07-30 10:41:47 -0700

    delete commented out code blocks

--- go/tasks/plugins/k8s/sagemaker/config/config.go
+++ go/tasks/plugins/k8s/sagemaker/config/config.go
@@ -6,24 +6,6 @@ import pluginsConfig "github.com/lyft/flyteplugins/go/tasks/config"
 
 const sagemakerConfigSectionKey = "sagemaker"
 
-//var (
-//	defaultConfig = Config{
-//		RoleArn: "default",
-//		Region:  "us-east-1",
-//		// https://docs.aws.amazon.com/sagemaker/latest/dg/sagemaker-algo-docker-registry-paths.html
-//		AlgorithmPrebuiltImages: map[string]map[string]map[string]string{
-//			"xgboost": {
-//				"us-east-1": {
-//					"0.72": "811284229777.dkr.ecr.us-east-1.amazonaws.com/xgboost:latest",
-//					"0.90": "683313688378.dkr.ecr.us-east-1.amazonaws.com/xgboost:latest",
-//				},
-//			},
-//		},
-//	}
-//
-//	sagemakerConfigSection = pluginsConfig.MustRegisterSubSection(sagemakerConfigSectionKey, &defaultConfig)
-//)
-
 var (
 	defaultConfig = Config{
 		RoleArn: "default",
@@ -68,13 +50,6 @@ type VersionConfig struct {
 	Version string `json:"version" pflag:",version of the algorithm"`
 	Image   string `json:"image" pflag:",Image URI of the algorithm"`
 }
-
-//// Sagemaker plugin configs
-//type Config struct {
-//	RoleArn                 string                                  `json:"roleArn" pflag:",The role the SageMaker plugin uses to communicate with the SageMaker service"`
-//	Region                  string                                  `json:"region" pflag:",The AWS region the SageMaker plugin communicates to"`
-//	AlgorithmPrebuiltImages map[string]map[string]map[string]string `json:"algorithmPrebuiltImages" pflag:"-,A map of maps containing the version to pre-built image for each supported algorithm"`
-//}
 
 // Retrieves the current config value or default.
 func GetSagemakerConfig() *Config {
