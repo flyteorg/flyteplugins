7ffa7948566fa39234e77a88dec2c6dcd7da0a11
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-08-12 11:41:11 -0700

    checking key existence in GetRole()

--- go/tasks/plugins/awsutils/awsutils.go
+++ go/tasks/plugins/awsutils/awsutils.go
@@ -4,7 +4,9 @@ import "context"
 
 func GetRole(_ context.Context, roleAnnotationKey string, annotations map[string]string) string {
 	if len(roleAnnotationKey) > 0 {
-		return annotations[roleAnnotationKey]
+		if role, found := annotations[roleAnnotationKey]; found {
+			return role
+		}
 	}
 
 	return ""
