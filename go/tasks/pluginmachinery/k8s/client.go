c8b7523407e2bbf193aba922fdb82849c40e843a
Filipe Regadas <filiperegadas@gmail.com>
2021-03-09 10:46:44 +0000

    format
    Signed-off-by: Filipe Regadas <filiperegadas@gmail.com>

--- go/tasks/pluginmachinery/k8s/client.go
+++ go/tasks/pluginmachinery/k8s/client.go
@@ -56,36 +56,36 @@ func (c fallbackClientReader) List(ctx context.Context, list client.ObjectList, 
 }
 
 type fallbackClientBuilder struct {
- 	uncached []client.Object
- }
+	uncached []client.Object
+}
 
- func (f *fallbackClientBuilder) WithUncached(objs ...client.Object) cluster.ClientBuilder {
- 	f.uncached = append(f.uncached, objs...)
- 	return f
- }
+func (f *fallbackClientBuilder) WithUncached(objs ...client.Object) cluster.ClientBuilder {
+	f.uncached = append(f.uncached, objs...)
+	return f
+}
 
- func (f fallbackClientBuilder) Build(cache cache.Cache, config *rest.Config, options client.Options) (client.Client, error) {
- 	c, err := client.New(config, options)
- 	if err != nil {
- 		return nil, err
- 	}
+func (f fallbackClientBuilder) Build(cache cache.Cache, config *rest.Config, options client.Options) (client.Client, error) {
+	c, err := client.New(config, options)
+	if err != nil {
+		return nil, err
+	}
 
- 	return client.NewDelegatingClient(client.NewDelegatingClientInput{
- 		Client: c,
- 		CacheReader: fallbackClientReader{
- 			orderedClients: []client.Reader{cache, c},
- 		},
- 		UncachedObjects: f.uncached,
- 		// TODO figure out if this should be true?
- 		// CacheUnstructured: true,
- 	})
- }
+	return client.NewDelegatingClient(client.NewDelegatingClientInput{
+		Client: c,
+		CacheReader: fallbackClientReader{
+			orderedClients: []client.Reader{cache, c},
+		},
+		UncachedObjects: f.uncached,
+		// TODO figure out if this should be true?
+		// CacheUnstructured: true,
+	})
+}
 
 // Creates a new k8s client that uses the cached client for reads and falls back to making API
 // calls if it failed. Write calls will always go to raw client directly.
 func NewFallbackClientBuilder() cluster.ClientBuilder {
- 	return &fallbackClientBuilder{}
- }
+	return &fallbackClientBuilder{}
+}
 
 type Options struct {
 	MapperProvider func(*rest.Config) (meta.RESTMapper, error)
