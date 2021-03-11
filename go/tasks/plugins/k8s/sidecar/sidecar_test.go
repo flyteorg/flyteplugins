d99df4a2116ea248fc8374c86189a347a9e010f2
Anand Swaminathan <aswaminathan@lyft.com>
2021-03-11 14:30:11 -0800

    code-cov
    Signed-off-by: Anand Swaminathan <aswaminathan@lyft.com>

--- go/tasks/plugins/k8s/sidecar/sidecar_test.go
+++ go/tasks/plugins/k8s/sidecar/sidecar_test.go
@@ -63,9 +63,8 @@ func dummyContainerTaskMetadata(resources *v1.ResourceRequirements) pluginsCore.
 		Name: "blah",
 	})
 	taskMetadata.On("IsInterruptible").Return(true)
-	taskMetadata.On("GetSecurityContext").Return(core.SecurityContext{
-		RunAs: &core.Identity{K8SServiceAccount: "service-account"},
-	})
+	taskMetadata.On("GetSecurityContext").Return(core.SecurityContext{})
+	taskMetadata.On("GetK8sServiceAccount").Return("service-account")
 	taskMetadata.On("GetOwnerID").Return(types.NamespacedName{
 		Namespace: "test-namespace",
 		Name:      "test-owner-name",
