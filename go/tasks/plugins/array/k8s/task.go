95f6e5c535fb64042b590f95a6da9a5187d1eb28
Anmol Khurana <akhurana@lyft.com>
2021-07-16 14:01:12 -0700

    Fix finalizer name
    Signed-off-by: Anmol Khurana <akhurana@lyft.com>

--- go/tasks/plugins/array/k8s/task.go
+++ go/tasks/plugins/array/k8s/task.go
@@ -46,7 +46,7 @@ const (
 	LaunchReturnState
 )
 
-const finalizer = "array.k8s.finalizers.flyte.k8s.io"
+const finalizer = "flyte/array"
 
 func addPodFinalizer(pod *corev1.Pod) *corev1.Pod {
 	pod.Finalizers = append(pod.Finalizers, finalizer)
