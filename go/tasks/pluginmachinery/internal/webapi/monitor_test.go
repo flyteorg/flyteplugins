fb59e2d3e6d1f511d5eb6bf16b6df9c251b911a5
Haytham Abuelfutuh <haytham@afutuh.com>
2021-09-23 11:27:39 -0700

    Fix unit test
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- go/tasks/pluginmachinery/internal/webapi/monitor_test.go
+++ go/tasks/pluginmachinery/internal/webapi/monitor_test.go
@@ -2,6 +2,7 @@ package webapi
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -35,7 +36,11 @@ func Test_monitor(t *testing.T) {
 	client := &internalMocks.Client{}
 	client.OnStatusMatch(ctx, mock.Anything).Return(core2.PhaseInfoSuccess(nil), nil)
 
+	wg := sync.WaitGroup{}
+	wg.Add(4)
 	cacheObj, err := cache.NewAutoRefreshCache(rand.String(5), func(ctx context.Context, batch cache.Batch) (updatedBatch []cache.ItemSyncResponse, err error) {
+		wg.Done()
+		t.Logf("Syncing Item [%+v]", batch[0])
 		return []cache.ItemSyncResponse{
 			{
 				ID:     batch[0].GetID(),
@@ -43,9 +48,16 @@ func Test_monitor(t *testing.T) {
 				Action: cache.Update,
 			},
 		}, nil
-	}, workqueue.DefaultControllerRateLimiter(), time.Second, 10, 10, promutils.NewTestScope())
+	}, workqueue.DefaultControllerRateLimiter(), time.Second, 1, 10, promutils.NewTestScope())
 	assert.NoError(t, err)
-	cacheObj.GetOrCreate("generated_name", CacheItem{Resource: "fake_resource"})
+
+	assert.NoError(t, cacheObj.Start(ctx))
+
+	_, err = cacheObj.GetOrCreate("generated_name2", CacheItem{Resource: "fake_resource2"})
+	assert.NoError(t, err)
+
+	_, err = cacheObj.GetOrCreate("generated_name", CacheItem{Resource: "fake_resource"})
+	assert.NoError(t, err)
 
 	s := &State{}
 	newState, phaseInfo, err := monitor(ctx, tCtx, client, cacheObj, s)
@@ -54,7 +66,8 @@ func Test_monitor(t *testing.T) {
 	assert.NotNil(t, phaseInfo)
 	assert.Equal(t, core2.PhaseSuccess.String(), phaseInfo.Phase().String())
 
-	cachedItem, err := cacheObj.GetOrCreate("generated_name", CacheItem{Resource: "fake_resource"})
+	wg.Wait()
+	cachedItem, err := cacheObj.GetOrCreate("generated_name", CacheItem{Resource: "new_resource"})
 	assert.NoError(t, err)
-	assert.Equal(t, core2.PhaseSuccess.String(), cachedItem.(CacheItem).Phase.String())
+	assert.Equal(t, "new_resource", cachedItem.(CacheItem).Resource.(string))
 }
