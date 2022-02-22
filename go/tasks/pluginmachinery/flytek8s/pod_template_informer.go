9106fca5ab082e09ae3de9b7edb6ff361ab28072
Daniel Rammer <daniel@union.ai>
2022-02-21 19:14:21 -0600

    implemente a simple cycle test
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- go/tasks/pluginmachinery/flytek8s/pod_template_informer.go
+++ go/tasks/pluginmachinery/flytek8s/pod_template_informer.go
@@ -2,10 +2,14 @@ package flytek8s
 
 import (
 	"context"
+	"time"
 
-	"github.com/flyteorg/flyteplugins/go/tasks/pluginmachinery/core"
+	"github.com/flyteorg/flytestdlib/logger"
 
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/tools/cache"
 )
 
 /*var podTemplateInformer := podTemplateInformer{
@@ -13,22 +17,58 @@ import (
 }*/
 
 type podTemplateInformer struct {
-	kubeClient      core.KubeClient
-	podTemplate     *v1.PodTemplate
-	podTemplateName string
-	stopChan        chan struct{}
+	kubeClient           kubernetes.Interface
+	podTemplate          *v1.PodTemplate
+	podTemplateName      string
+	podTemplateNamespace string
+	stopChan             chan struct{}
 }
 
-func (p podTemplateInformer) start(ctx context.Context) error {
-	if p.kubeClient != nil && p.podTemplateName != "" {
-		//p.stopChan = chan
-		//go informerFactory.Start(p.stopChan)
+func (p *podTemplateInformer) start(ctx context.Context) error {
+	if p.stopChan == nil && p.kubeClient != nil && p.podTemplateName != "" && p.podTemplateNamespace != "" {
+		logger.Infof(ctx, "STARTING")
+		// TODO hamersaw - parameterize defaultResync
+		informerFactory := informers.NewSharedInformerFactoryWithOptions(p.kubeClient, 1*time.Second, informers.WithNamespace(p.podTemplateNamespace))
+
+		informerFactory.Core().V1().PodTemplates().Informer().AddEventHandler(
+			cache.ResourceEventHandlerFuncs{
+				AddFunc: func(obj interface{}) {
+					podTemplate, ok := obj.(*v1.PodTemplate)
+					logger.Infof(context.TODO(), "ADDED OBJECT %v '%s'", ok, podTemplate.Name)
+					if ok && podTemplate.Name == p.podTemplateName {
+						p.podTemplate = podTemplate
+						logger.Infof(context.TODO(), "SET OBJECT %v '%s'", ok, podTemplate.Name)
+						//logger.Infof(context.TODO(), "Deletion triggered for %v", name)
+					}
+				},
+				UpdateFunc: func(old, new interface{}) {
+					podTemplate, ok := new.(*v1.PodTemplate)
+					logger.Infof(context.TODO(), "UPDATED OBJECT '%s'", podTemplate.Name)
+					if ok && podTemplate.Name == p.podTemplateName {
+						p.podTemplate = podTemplate
+						logger.Infof(context.TODO(), "SET OBJECT '%s'", podTemplate.Name)
+						//logger.Infof(context.TODO(), "Deletion triggered for %v", name)
+					}
+				},
+				DeleteFunc: func(obj interface{}) {
+					podTemplate, ok := obj.(*v1.PodTemplate)
+					logger.Infof(context.TODO(), "DELETED OBJECT")
+					if ok && podTemplate.Name == p.podTemplateName {
+						p.podTemplate = nil
+						//logger.Infof(context.TODO(), "Deletion triggered for %v", name)
+					}
+				},
+			})
+
+		p.stopChan = make(chan struct{})
+		go informerFactory.Start(p.stopChan)
 	}
 
 	return nil
 }
 
-func (p podTemplateInformer) stop(ctx context.Context) error {
+func (p *podTemplateInformer) stop(ctx context.Context) error {
+	logger.Infof(ctx, "STOPPING")
 	if p.stopChan != nil {
 		close(p.stopChan)
 		p.stopChan = nil
--- go/tasks/pluginmachinery/flytek8s/pod_template_informer_test.go
+++ go/tasks/pluginmachinery/flytek8s/pod_template_informer_test.go
@@ -2,16 +2,65 @@ package flytek8s
 
 import (
 	"context"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+var (
+	podTemplate = &v1.PodTemplate{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "defaultPodTemplate",
+			Namespace: "defaultNamespace",
+		},
+		Template: v1.PodTemplateSpec{
+			Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					v1.Container{
+						Command: []string{"flytepropeller"},
+						Args:    []string{"--config", "/etc/flyte/config/*.yaml"},
+					},
+				},
+			},
+		},
+	}
 )
 
 func TestPodTemplateInformer(t *testing.T) {
 	ctx := context.TODO()
 
-	informer := podTemplateInformer{}
+	kubeClient := fake.NewSimpleClientset()
+	informer := podTemplateInformer{
+		kubeClient:           kubeClient,
+		podTemplateName:      podTemplate.Name,
+		podTemplateNamespace: podTemplate.Namespace,
+	}
 
 	assert.NoError(t, informer.start(ctx))
+
+	// create the podTemplate
+	_, err := kubeClient.CoreV1().PodTemplates(podTemplate.Namespace).Create(ctx, podTemplate, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	time.Sleep(10 * time.Millisecond)
+	assert.NotNil(t, informer.podTemplate)
+	assert.True(t, reflect.DeepEqual(podTemplate, informer.podTemplate))
+
+	// TODO update the podTemplate in the namespace
+	// TODO validate update
+
+	// delete the podTemplate in the namespace
+	err = kubeClient.CoreV1().PodTemplates(podTemplate.Namespace).Delete(ctx, podTemplate.Name, metav1.DeleteOptions{})
+	assert.NoError(t, err)
+
+	time.Sleep(10 * time.Millisecond)
+	assert.Nil(t, informer.podTemplate)
+
 	assert.NoError(t, informer.stop(ctx))
 }
