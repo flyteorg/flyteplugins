3cfba8e56801c4d41d37adec4e105fa902d85ca5
Daniel Rammer <daniel@union.ai>
2022-03-01 10:50:38 -0600

    fixed setting default configuration
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- go/tasks/config/config.go
+++ go/tasks/config/config.go
@@ -9,14 +9,18 @@ import (
 const configSectionKey = "plugins"
 
 var (
+	defaultConfig = &Config{
+		EnabledPlugins: []string{"*"},
+	}
+
 	// Root config section. If you are a plugin developer and your plugin needs a config, you should register
 	// your config as a subsection for this root section.
-	rootSection = config.MustRegisterSection(configSectionKey, &Config{})
+	rootSection = config.MustRegisterSection(configSectionKey, defaultConfig)
 )
 
 // Top level plugins config.
 type Config struct {
-	EnabledPlugins []string `json:"enabled-plugins" pflag:"[]string{\"*\"},Deprecated - List of enabled plugins, default value is to enable all plugins."`
+	EnabledPlugins []string `json:"enabled-plugins" pflag:",Deprecated - List of enabled plugins, default value is to enable all plugins."`
 }
 
 // Retrieves the current config value or default.
