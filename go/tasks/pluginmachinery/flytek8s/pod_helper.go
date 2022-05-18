2f4b583481621d0dc33b6f5c94f5366d3dcbb822
Robert Everson <reverson@lyft.com>
2022-05-18 15:58:55 -0700

    Use container arch instead of metadata arch
    Signed-off-by: Robert Everson <reverson@lyft.com>

--- go/tasks/pluginmachinery/flytek8s/pod_helper.go
+++ go/tasks/pluginmachinery/flytek8s/pod_helper.go
@@ -98,9 +98,8 @@ func UpdatePod(taskExecutionMetadata pluginsCore.TaskExecutionMetadata,
 
 // Updates the base pod spec used to execute tasks. This is configured with plugins and task metadata-specific options
 func UpdatePodWithInterruptibleFlag(taskExecutionMetadata pluginsCore.TaskExecutionMetadata,
-	resourceRequirements []v1.ResourceRequirements, podSpec *v1.PodSpec, omitInterruptible bool) {
+	resourceRequirements []v1.ResourceRequirements, podSpec *v1.PodSpec, omitInterruptible bool, architecture core.Container_Architecture) {
 	isInterruptible := !omitInterruptible && taskExecutionMetadata.IsInterruptible()
-	architecture := taskExecutionMetadata.GetArchitecture()
 	if len(podSpec.RestartPolicy) == 0 {
 		podSpec.RestartPolicy = v1.RestartPolicyNever
 	}
@@ -161,7 +160,7 @@ func ToK8sPodSpecWithInterruptible(ctx context.Context, tCtx pluginsCore.TaskExe
 	pod := &v1.PodSpec{
 		Containers: containers,
 	}
-	UpdatePodWithInterruptibleFlag(tCtx.TaskExecutionMetadata(), []v1.ResourceRequirements{c.Resources}, pod, omitInterruptible)
+	UpdatePodWithInterruptibleFlag(tCtx.TaskExecutionMetadata(), []v1.ResourceRequirements{c.Resources}, pod, omitInterruptible, architecture)
 
 	if err := AddCoPilotToPod(ctx, config.GetK8sPluginConfig().CoPilot, pod, task.GetInterface(), tCtx.TaskExecutionMetadata(), tCtx.InputReader(), tCtx.OutputWriter(), task.GetContainer().GetDataConfig()); err != nil {
 		return nil, err
