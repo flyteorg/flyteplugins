d70bce4c7bf764d0694e920c90209a7b561602bb
Kevin Su <pingsutw@apache.org>
2022-11-29 14:43:34 -0800

    address comment
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- go/tasks/plugins/array/awsbatch/executor.go
+++ go/tasks/plugins/array/awsbatch/executor.go
@@ -80,19 +80,7 @@ func (e Executor) Handle(ctx context.Context, tCtx core.TaskExecutionContext) (c
 		pluginState, err = LaunchSubTasks(ctx, tCtx, e.jobStore, pluginConfig, pluginState, e.metrics)
 
 	case arrayCore.PhaseCheckingSubTaskExecutions:
-		// Check that the taskTemplate is valid
-		var taskTemplate *idlCore.TaskTemplate
-		taskTemplate, err = tCtx.TaskReader().Read(ctx)
-		if err != nil {
-			return core.UnknownTransition, errors.Wrapf(errors.CorruptedPluginState, err, "Failed to read task template")
-		} else if taskTemplate == nil {
-			return core.UnknownTransition, errors.Errorf(errors.BadTaskSpecification, "Required value not set, taskTemplate is nil")
-		}
-		retry := toRetryStrategy(ctx, toBackoffLimit(taskTemplate.Metadata), pluginConfig.MinRetries, pluginConfig.MaxRetries)
-
-		pluginState, err = CheckSubTasksState(ctx, tCtx.TaskExecutionMetadata(),
-			tCtx.OutputWriter().GetOutputPrefixPath(), tCtx.OutputWriter().GetRawOutputPrefix(),
-			e.jobStore, tCtx.DataStore(), pluginConfig, pluginState, e.metrics, *retry.Attempts)
+		pluginState, err = CheckSubTasksState(ctx, tCtx, e.jobStore, pluginConfig, pluginState, e.metrics)
 
 	case arrayCore.PhaseAssembleFinalOutput:
 		pluginState.State, err = array.AssembleFinalOutputs(ctx, e.outputAssembler, tCtx, arrayCore.PhaseSuccess, version, pluginState.State)
