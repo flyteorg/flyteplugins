b91bc370b248b1ae671ad08652666c84cb7fbdc5
Kevin Su <pingsutw@apache.org>
2023-01-19 22:17:42 -0800

    wip
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- go/tasks/pluginmachinery/internal/webapi/launcher.go
+++ go/tasks/pluginmachinery/internal/webapi/launcher.go
@@ -16,7 +16,7 @@ func launch(ctx context.Context, p webapi.AsyncPlugin, tCtx core.TaskExecutionCo
 	rMeta, r, err := p.Create(ctx, tCtx)
 	if err != nil {
 		logger.Errorf(ctx, "Failed to create resource. Error: %v", err)
-		return nil, core.PhaseInfo{}, err
+		return state, core.PhaseInfoFailure("Failed to create resource", err.Error(), nil), nil
 	}
 
 	// If the plugin also returned the created resource, check to see if it's already in a terminal state.
