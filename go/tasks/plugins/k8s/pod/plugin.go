6e9ee1fb1440b04428be9e4314bb80ef7717486a
Jeev B <jeevb@users.noreply.github.com>
2023-07-13 11:15:41 -0700

    fix

--- go/tasks/plugins/k8s/pod/plugin.go
+++ go/tasks/plugins/k8s/pod/plugin.go
@@ -166,9 +166,9 @@ func (plugin) GetTaskPhaseWithLogs(ctx context.Context, pluginContext k8s.Plugin
 		ReportedAt: &reportedAt,
 	}
 
-	taskExecId := pluginContext.TaskExecutionMetadata().GetTaskExecutionID().GetID()
+	taskExecID := pluginContext.TaskExecutionMetadata().GetTaskExecutionID().GetID()
 	if pod.Status.Phase != v1.PodPending && pod.Status.Phase != v1.PodUnknown {
-		taskLogs, err := logs.GetLogsForContainerInPod(ctx, logPlugin, &taskExecId, pod, 0, logSuffix, extraLogTemplateVars...)
+		taskLogs, err := logs.GetLogsForContainerInPod(ctx, logPlugin, &taskExecID, pod, 0, logSuffix, extraLogTemplateVars...)
 		if err != nil {
 			return pluginsCore.PhaseInfoUndefined, err
 		}
