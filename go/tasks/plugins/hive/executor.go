1fc603bddeb0be2f17809f2f43dcf1700c94ed60
Daniel Rammer <daniel@union.ai>
2023-09-26 11:27:35 -0500

    Revert "deprecated TransitionTypeBarrier (#336)"
    This reverts commit 4f02c2ca8d461538e43a1cd6250bcfd23cd448c4.
    
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- go/tasks/plugins/hive/executor.go
+++ go/tasks/plugins/hive/executor.go
@@ -65,7 +65,7 @@ func (q QuboleHiveExecutor) Handle(ctx context.Context, tCtx core.TaskExecutionC
 		return core.UnknownTransition, err
 	}
 
-	return core.DoTransition(phaseInfo), nil
+	return core.DoTransitionType(core.TransitionTypeBarrier, phaseInfo), nil
 }
 
 func (q QuboleHiveExecutor) Abort(ctx context.Context, tCtx core.TaskExecutionContext) error {
